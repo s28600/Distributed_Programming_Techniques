261cee25f1fb621d9ff0cf1dde325bf4af95f8eb
Illya Moskalenko <illya.moskalenko@gmail.com>
2024-03-23 15:11:18 +0100

    Implemented weather api call and json output

--- TPO2_MI_S28600/src/zad1/Service.java
+++ TPO2_MI_S28600/src/zad1/Service.java
@@ -7,5 +7,51 @@
 package zad1;
 
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Locale;
+
 public class Service {
-}  
+    String countryName;
+    String countryISOcode;
+    private final String WEATHER_API_LINK = "https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid=6aa72ca1829d3653da87b87a89438056";
+
+    public Service(String countryName) {
+        this.countryName = countryName;
+
+        Locale temp;
+        for (String code : Locale.getISOCountries()){
+            temp = new Locale("EN", code);
+            if (temp.getDisplayCountry().equals(countryName)) countryISOcode = code;
+        }
+    }
+
+    public String getWeather(String cityName) {
+        StringBuilder out = new StringBuilder();
+        try {
+            URL url = new URL(WEATHER_API_LINK
+                    .replace("{city name}", cityName)
+                    .replace("{country code}", countryISOcode));
+            BufferedReader bfin = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line;
+            while ((line = bfin.readLine()) != null) {
+                out.append(line);
+            }
+            bfin.close();
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        System.out.println(out);
+        return out.toString();
+    }
+
+    public double getRateFor(String currencyName){
+        return 0;
+    }
+
+    public double getNBPRate(){
+        return 0;
+    }
+}
