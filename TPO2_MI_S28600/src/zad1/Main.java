b52292685490bb8397660153f684f939ad9d3038
Illya Moskalenko <illya.moskalenko@gmail.com>
2024-03-23 21:00:09 +0100

    Simple json library added, implemented json parsing, methods in Service class completed

--- TPO2_MI_S28600/src/zad1/Main.java
+++ TPO2_MI_S28600/src/zad1/Main.java
@@ -11,8 +11,11 @@ public class Main {
   public static void main(String[] args) {
     Service s = new Service("France");
     String weatherJson = s.getWeather("Paris");
+    System.out.println(weatherJson);
     Double rate1 = s.getRateFor("USD");
+    System.out.println(rate1);
     Double rate2 = s.getNBPRate();
+    System.out.println(rate2);
     // ...
     // część uruchamiająca GUI
   }
--- TPO2_MI_S28600/src/zad1/Service.java
+++ TPO2_MI_S28600/src/zad1/Service.java
@@ -7,6 +7,8 @@
 package zad1;
 
 
+import com.github.cliftonlabs.json_simple.*;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -19,8 +21,6 @@ public class Service {
     private final String WEATHER_API = "https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid=6aa72ca1829d3653da87b87a89438056&units=metric";
     private final String CURRENCY_API = "https://v6.exchangerate-api.com/v6/7602de87df2af764f1d1b142/latest/";
     private final String NBP_API = "http://api.nbp.pl/api/exchangerates/rates/{table}/{code}?format=json";
-    Currency currency;
-
 
     public Service(String countryName) {
         for (Locale loc : Locale.getAvailableLocales()){
@@ -30,10 +30,9 @@ public class Service {
         if(country == null){
             throw new RuntimeException("Wrong or unsupported country entered, please try again.");
         }
-        currency = Currency.getInstance(country);
     }
 
-    private String getJSON(String API) throws IOException {
+    private String getJson(String API) throws IOException {
         URL url = new URL(API);
         StringBuilder out = new StringBuilder();
         BufferedReader bfin = new BufferedReader(new InputStreamReader(url.openStream()));
@@ -42,37 +41,41 @@ public class Service {
             out.append(line);
         }
         bfin.close();
-        System.out.println(out);
+        //System.out.println(out);
         return out.toString();
     }
 
     public String getWeather(String cityName) {
         try {
-            return getJSON(WEATHER_API
+            return getJson(WEATHER_API
                     .replace("{city name}", cityName)
                     .replace("{country code}", country.getISO3Country()));
-        } catch (IOException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
     public double getRateFor(String currencyName){
         try {
-            getJSON(CURRENCY_API + currency);
-            return 0;
-        } catch (IOException e) {
+            String json = getJson(CURRENCY_API + Currency.getInstance(country));
+            JsonObject obj = (JsonObject) Jsoner.deserialize(json);
+            return Double.parseDouble(((JsonObject)obj.get("conversion_rates")).get(currencyName).toString());
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
     public double getNBPRate(){
-        if (currency.toString().equals("PLN")){
+        if (Currency.getInstance(country).toString().equals("PLN")){
             return 1;
         } else {
             try {
-                getJSON(NBP_API.replace("{table}", "A").replace("{code}", currency.toString()));
-                return 0;
-            } catch (IOException e) {
+                String json = getJson(NBP_API.replace("{table}", "A")
+                        .replace("{code}", Currency.getInstance(country).toString()));
+                JsonObject obj = (JsonObject) Jsoner.deserialize(json);
+                JsonObject rates = (JsonObject)((JsonArray)obj.get("rates")).get(0);
+                return 1/Double.parseDouble(rates.get("mid").toString());
+            } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         }
