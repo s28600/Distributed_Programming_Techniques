648ccf4872ba4e38861f7b632ebfed289e866640
Illya Moskalenko <illya.moskalenko@gmail.com>
2024-03-23 19:25:07 +0100

    Implemented api calls for weather, exchange rates and NBP rates

--- TPO2_MI_S28600/src/zad1/Service.java
+++ TPO2_MI_S28600/src/zad1/Service.java
@@ -8,50 +8,73 @@ package zad1;
 
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Currency;
 import java.util.Locale;
 
 public class Service {
-    String countryName;
-    String countryISOcode;
-    private final String WEATHER_API_LINK = "https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid=6aa72ca1829d3653da87b87a89438056";
+    Locale country;
+    private final String WEATHER_API = "https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid=6aa72ca1829d3653da87b87a89438056&units=metric";
+    private final String CURRENCY_API = "https://v6.exchangerate-api.com/v6/7602de87df2af764f1d1b142/latest/";
+    private final String NBP_API = "http://api.nbp.pl/api/exchangerates/rates/{table}/{code}?format=json";
+    Currency currency;
+
 
     public Service(String countryName) {
-        this.countryName = countryName;
-
-        Locale temp;
-        for (String code : Locale.getISOCountries()){
-            temp = new Locale("EN", code);
-            if (temp.getDisplayCountry().equals(countryName)) countryISOcode = code;
+        for (Locale loc : Locale.getAvailableLocales()){
+            if (loc.getDisplayCountry().equals(countryName))
+                country = loc;
         }
+        if(country == null){
+            throw new RuntimeException("Wrong or unsupported country entered, please try again.");
+        }
+        currency = Currency.getInstance(country);
     }
 
-    public String getWeather(String cityName) {
+    private String getJSON(String API) throws IOException {
+        URL url = new URL(API);
         StringBuilder out = new StringBuilder();
-        try {
-            URL url = new URL(WEATHER_API_LINK
-                    .replace("{city name}", cityName)
-                    .replace("{country code}", countryISOcode));
-            BufferedReader bfin = new BufferedReader(new InputStreamReader(url.openStream()));
-            String line;
-            while ((line = bfin.readLine()) != null) {
-                out.append(line);
-            }
-            bfin.close();
-        } catch (Exception e){
-            throw new RuntimeException(e);
+        BufferedReader bfin = new BufferedReader(new InputStreamReader(url.openStream()));
+        String line;
+        while ((line = bfin.readLine()) != null) {
+            out.append(line);
         }
+        bfin.close();
         System.out.println(out);
         return out.toString();
     }
 
+    public String getWeather(String cityName) {
+        try {
+            return getJSON(WEATHER_API
+                    .replace("{city name}", cityName)
+                    .replace("{country code}", country.getISO3Country()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public double getRateFor(String currencyName){
-        return 0;
+        try {
+            getJSON(CURRENCY_API + currency);
+            return 0;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public double getNBPRate(){
-        return 0;
+        if (currency.toString().equals("PLN")){
+            return 1;
+        } else {
+            try {
+                getJSON(NBP_API.replace("{table}", "A").replace("{code}", currency.toString()));
+                return 0;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
