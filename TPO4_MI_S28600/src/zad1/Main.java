880f40de7abd2b0efe9d10e84108d437aaf7e81d
s28600 <s28600@pjwstk.edu.pl>
2024-05-13 11:22:12 +0200

    AdminUI started

--- TPO4_MI_S28600/src/zad1/Main.java
+++ TPO4_MI_S28600/src/zad1/Main.java
@@ -5,5 +5,6 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         ClientUI clientUi = new ClientUI(new Client());
+        AdminUI adminUI = new AdminUI(new Administrator());
     }
 }
